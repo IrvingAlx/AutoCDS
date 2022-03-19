48942a4f43922017f7639b29c4abcda51bc19cea
Irving Alejandro <a2169690@correo.uia.mx>
2022-03-18 21:29:42 -0600

    Completed Code

---
+++ AutoCDS.py
@@ -0,0 +1,36 @@
+from webbrowser import Chrome
+from PIL import Image
+import pyautogui
+
+import pytesseract
+
+pyautogui.hotkey("win")
+pyautogui.sleep(2)
+pyautogui.typewrite("chrome")
+pyautogui.hotkey("enter")
+pyautogui.sleep(5)
+pyautogui.typewrite("https://cuestionariodesalud.ibero.mx/#/login")
+pyautogui.hotkey("enter")
+
+pyautogui.sleep(3)
+
+pyautogui.hotkey("f5")
+
+pyautogui.sleep(1)
+
+xyhw = pyautogui.locateOnScreen("conver2ted.png")
+print(xyhw)
+xyhw = pyautogui.center(xyhw)
+
+pyautogui.click(xyhw)
+
+pyautogui.sleep(2)
+
+for pos in pyautogui.locateAllOnScreen("conver3ted.png"):
+   print(pos)
+   pyautogui.click(pos)
+
+xyhw = pyautogui.locateOnScreen("conver4ted.png")
+xyhw = pyautogui.center(xyhw)
+
+pyautogui.click(xyhw)
